1914cf1db0de2bdc6cd616a9605722df488c8ed3
Joanna Dyczka <asia.dyczka@gmail.com>
2018-04-10 11:54:56 +0200

    przed oddaniem

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,78 @@
-const gulp = require('gulp');
+
+const   gulp = require('gulp'),
+        fs = require('fs'),
+        download = require('gulp-download'),
+        request = require('request'),
+        cheerio = require('cheerio'),
+        fsc = require('fs-cheerio'),
+        argv = require('yargs').argv,
+        gutil = require('gulp-util')
+        sass = require('gulp-sass')
+        sourcemaps = require('gulp-sourcemaps'),
+        autoprefixer = require('gulp-autoprefixer'),
+        browserSync = require('browser-sync').create(),
+        del = require('del'),
+        run = require('gulp-run');
+
+var prodNum = argv.nr;
+
+gulp.task('clean', function() {
+    return del.sync(['dist']);
+});
+
+gulp.task('get', function() {
+    request('https://restcountries.eu/rest/v2/all', function(err,resp,data) {
+        if (!err) {
+            fs.writeFile( 'src/countriesInfo.json', data, function(err){} );
+        } else {
+            gutil.log('ERR', err);
+        }
+    });
+});
+
+gulp.task('sass', function() {
+    return gulp.src('src/style.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('xml', function() {
+    return run('node ./src/generateXml').exec();
+});
+
+gulp.task('default', function () {
+    gulp.start(
+        'sass',
+        'xml'
+    );
+});
+
+
+gulp.task('watch-sass', ['sass'], function(done){
+    browserSync.reload();
+    done();
+});
+
+gulp.task('watch-xml', ['xml'], function(done){
+    browserSync.reload();
+    done();
+});
+    
+gulp.task('serve', ['default'], function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist',
+            //directory: true
+            index: 'countries.xml'
+        }
+    });
+
+    gulp.watch('src/**/*.scss', ['watch-sass']); 
+    gulp.watch('src/generateXml.js', ['watch-xml']); 
+});
---
+++ src/generateXml.js
@@ -0,0 +1,90 @@
+var fs = require('fs');
+var countries;
+fs.readFile('./src/countriesInfo.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    countries = JSON.parse(data);
+    //console.log(countries);
+
+    var xmlString = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<!DOCTYPE document SYSTEM "doc.dtd">
+<?xml-stylesheet href="style.css" type="text/css" ?>
+<document>
+    <document-info>
+        <title>Countries of the World</title>
+        <author>Joanna Dyczka</author>
+        <date>20.03.2018</date>
+    </document-info>
+`;
+
+    var xmlCountryCodes = `
+    <country-codes>`;
+    var xmlCountries = `
+    <countries>
+`;
+
+    countries.forEach( c => {
+        var xmlTZones = '';
+        c.timezones.forEach( t => {
+            xmlTZones += `
+                <country-info-item info-item="timezone">${t}</country-info-item>`;
+        });
+        var xmlBor = '';
+        c.borders.forEach( b => {
+            xmlBor += `
+                <country-info-item info-item="border">${b}</country-info-item>`;
+        });
+        var xmlCurr = '';
+        c.currencies.forEach( c => {
+            xmlCurr += `
+                <country-info-item info-item="currency">
+                    <currency-symbol>${c.symbol}</currency-symbol>
+                    <currency-code>${c.code}</currency-code>
+                    <currency-name>${c.name}</currency-name>
+                </country-info-item>`;
+        });
+        var xmlLang = '';
+        c.languages.forEach( l => {
+            xmlLang += `
+                <country-info-item info-item="language">${l.name}</country-info-item>`;
+        });
+
+        xmlCountries += `        <country country-code="${c.alpha3Code}" tabindex="0">
+            <country-name>${c.name}</country-name>
+            <country-flag src="${c.flag}"><html xmlns="http://www.w3.org/1999/xhtml"><img alt="" src="${c.flag}" /></html></country-flag>
+            <country-info type="basic" info="region">${c.region}</country-info>
+            <country-info type="basic" info="capital">${c.capital}</country-info>
+            <country-info type="basic" info="population">${c.population}</country-info>
+            <country-info type="basic" info="area">${c.area}</country-info>
+            <country-info type="additional" info="timezones">${xmlTZones}
+            </country-info>
+            <country-info type="additional" info="borders">${xmlBor}</country-info>
+            <country-info type="additional" info="currencies">${xmlCurr}
+            </country-info>
+            <country-info type="additional" info="languages">${xmlLang}
+            </country-info>
+            <country-info type="additional" info="calling-code">${c.callingCodes[0]}</country-info>
+            <country-info type="additional" info="domain">${c.topLevelDomain}</country-info>
+        </country>
+`
+        xmlCountryCodes += `
+        <country-code code="${c.alpha3Code}"></country-code>`
+    });
+
+    xmlCountryCodes += `
+    </country-codes>
+    `;
+    xmlCountries += `
+    </countries>
+`;
+    
+    xmlString += xmlCountryCodes + xmlCountries + `</document>`;
+
+
+    var fs = require('fs');
+    fs.writeFile('./dist/countries.xml', xmlString, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
+});
